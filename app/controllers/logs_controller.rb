80b565a601b079edb4fa7983cb9f69f8d818400b
damdl96 <diegodm96@hotmail.com>
2020-02-23 22:05:53 -0600

    Feat: created logs controller and Create method.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,24 @@
 class LogsController < ApplicationController
+  def new
+    @log = Log.new
+  end
+
+  def show
+    @log = Log.find(params[:id])
+  end
+
+  def create
+    @activities = Activity.all
+    @log = Log.new(log_params)
+    if @log.save
+      redirect_to logs_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def log_params
+    params.require(:log).permit(:activityname, :timestart, :timeend, :notes)
+  end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,3 @@
 class Log < ApplicationRecord
+  
 end
