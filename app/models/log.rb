b4d3b881bb91fa4375a7e7536f80f64fadd9f84c
damdl96 <diegodm96@hotmail.com>
2020-02-24 17:43:51 -0600

    Feat: created log validations

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,14 @@
 class Log < ApplicationRecord
-  
+  validates :activityname, presence: { message: ' must not be blank' }
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if timeend.blank? || timestart.blank?
+
+    if timeend < timestart
+      errors.add(:timeend, 'must be after start time')
+    end
+  end
 end
