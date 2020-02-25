786e45ed1ca7775454f5e06acc4b193c4fd6162b
damdl96 <diegodm96@hotmail.com>
2020-02-24 20:03:17 -0600

    Feat: added a Read functionality of logs of the day.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -7,6 +7,10 @@ class LogsController < ApplicationController
     @logs = Log.all
   end
 
+  def today
+    @logs = Log.where('created_at BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+  end
+
   def show
     @log = Log.find(params[:id])
   end
@@ -26,7 +30,7 @@ class LogsController < ApplicationController
     @log.destroy
     redirect_to logs_path
   end
-
+  
   private
   def log_params
     params.require(:log).permit(:activityname, :timestart, :timeend, :notes)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :activities, :logs
+
+  get 'logstoday', to: 'logs#today'
 end
